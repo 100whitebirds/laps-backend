a10d0efee396f5589e4b9066157bb9c001c6b88f
Ruslan Zulufov <stucci@mail.ru>
2025-03-26 17:19:23 +0500

    update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -3939,7 +3939,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "94.247.129.222:8080",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "LAPS API",
